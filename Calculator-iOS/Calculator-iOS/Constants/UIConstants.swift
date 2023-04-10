d6594d3cc828bf4ba9954ff237ff51a2e1cb0ceb
Humberto <hgarcia@griddynamics.com>
2023-04-10 13:21:49 -0600

    Added son uiconstants for calculator color

--- Calculator-iOS/Calculator-iOS/Constants/UIConstants.swift
+++ Calculator-iOS/Calculator-iOS/Constants/UIConstants.swift
@@ -11,4 +11,11 @@ import UIKit
 struct UIConstants {
     static let transparent: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.33)
     static let backgroundColor = UIColor(named: "BackgroundColor")
+    
+    static let cleanButtonTextColor = UIColor(named: "CleanButtonTextColor")
+    static let operatorButtonTextColor = UIColor(named: "OperatorButtonTextColor")
+    static let digitButtonTextColor = UIColor(named: "DigitButtonTextColor")
+    static let equalButtonTextColor = UIColor(named: "EqualButtonTextColor")
+    static let zeroButtonTextColor = UIColor(named: "ZeroButtonTextColor")
+    static let decimalPointTextColor = UIColor(named: "DecimalPointTextColor")
 }
