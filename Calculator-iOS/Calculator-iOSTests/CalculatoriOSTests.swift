1b198f5bc60544d067835b6b453038c7a95d2169
Humberto <hgarcia@griddynamics.com>
2023-04-11 16:37:03 -0600

    Added unit testing for calcultorService

--- Calculator-iOS/Calculator-iOSTests/CalculatoriOSTests.swift
+++ Calculator-iOS/Calculator-iOSTests/CalculatoriOSTests.swift
@@ -5,26 +5,104 @@
 //  Created by Serhii Liamtsev on 4/9/22.
 //
 
-@testable import Calculator_iOS
 import XCTest
+@testable import Calculator_iOS_Dev
 
-final class CalculatoriOSTests: XCTestCase {
+class CalculatorServiceTests: XCTestCase {
+
+    var calculatorService: CalculatorService!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
+        calculatorService = CalculatorService()
     }
 
     override func tearDownWithError() throws {
+        calculatorService = nil
         try super.tearDownWithError()
     }
+    
+    // MARK: - ClearLastInput Cases
+    func testClearLastInput() {
+        // Given
+        calculatorService.setOperationsHistory("2+2")
 
-    func testExample() throws {
-        XCTAssert(true, "The true test")
+        // When
+        calculatorService.clearLastInput()
+
+        // Then
+        XCTAssertEqual(calculatorService.getOperationsHistory(), "2+")
+    }
+    
+    func testClearLastInputWithDecimals() {
+        // Given
+        calculatorService.setOperationsHistory("2.2")
+
+        // When
+        calculatorService.clearLastInput()
+
+        // Then
+        XCTAssertEqual(calculatorService.getOperationsHistory(), "2.")
+    }
+    
+    func testClearLastInputWithOperators() {
+        // Given
+        calculatorService.setOperationsHistory("2/5")
+
+        // When
+        calculatorService.clearLastInput()
+
+        // Then
+        XCTAssertEqual(calculatorService.getOperationsHistory(), "2/")
+    }
+    
+    func testClearLastInputWithOperatorsAndMissingDecimalNumber() {
+        // Given
+        calculatorService.setOperationsHistory("2.2/2.2")
+
+        // When
+        calculatorService.clearLastInput()
+
+        // Then
+        XCTAssertEqual(calculatorService.getOperationsHistory(), "2.2/2.")
+    }
+    
+    // MARK: - Setter and Getter for LastResult Cases
+    func testSetAndGetLastResult() {
+        // Given
+        let expectedResult: Double = 5.5
+
+        // When
+        calculatorService.setLastResult(expectedResult)
+        let result = calculatorService.getLastResult()
+
+        // Then
+        XCTAssertEqual(result, expectedResult)
     }
 
-    func testPerformanceExample() throws {
-        self.measure {
-        }
+    // MARK: - Setter and Getter for OperationsHistory Cases
+    func testSetAndGetOperationsHistory() {
+        // Given
+        let expectedHistory = "2.2+2.2"
+
+        // When
+        calculatorService.setOperationsHistory(expectedHistory)
+        let result = calculatorService.getOperationsHistory()
+
+        // Then
+        XCTAssertEqual(result, expectedHistory)
+    }
+
+    // MARK: - UpdateResult Cases
+    func testUpdateResult() {
+        // Given
+        calculatorService.setOperationsHistory("2.2+2.2")
+
+        // When
+        calculatorService.updateResult()
+
+        // Then
+        XCTAssertEqual(calculatorService.getLastResult(), 4)
     }
 
 }
